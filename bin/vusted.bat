cced868d415e7d1c0768b6cbd224a52924ace142
notomo <notomo.motono@gmail.com>
2020-09-26 09:12:52 +0900

    Fix to quit on module loading error

--- bin/vusted.bat
+++ bin/vusted.bat
@@ -1,7 +1,9 @@
+@echo off
+setlocal
 
 if "%VUSTED_NVIM%"== "" set VUSTED_NVIM=nvim
 
 %VUSTED_NVIM% --headless -u NONE -i NONE -n ^
     --cmd "let &runtimepath = getcwd() .. ',' .. &runtimepath" ^
     --cmd "let &runtimepath = fnamemodify(resolve('%~dp0'), ':h:h') .. ',' .. &runtimepath" ^
-    --cmd "call vusted#run()" -- %*
+    --cmd "call vusted#run()" --cmd "quitall!" -- %*
