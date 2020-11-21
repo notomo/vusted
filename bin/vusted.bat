ba4a1e3e57a65d78ff6bd6a8347609a5c1231a8c
notomo <notomo.motono@gmail.com>
2020-11-21 11:12:19 +0900

    Add VUSTED_ARGS to customize

--- bin/vusted.bat
+++ bin/vusted.bat
@@ -2,7 +2,8 @@
 setlocal
 
 if "%VUSTED_NVIM%"== "" set VUSTED_NVIM=nvim
+if "%VUSTED_ARGS%"== "" set VUSTED_ARGS=--headless -u NONE -i NONE -n
 
-%VUSTED_NVIM% --headless -u NONE -i NONE -n ^
+%VUSTED_NVIM% %VUSTED_ARGS% ^
     --cmd "execute 'source ' .. fnamemodify(resolve('%~dp0'), ':h') .. '/vusted_entry.vim'" ^
     -- %*
