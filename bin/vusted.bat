de7e8a73b2954ad0f91bcfba8380637e4aa2a0f8
notomo <notomo.motono@gmail.com>
2022-04-04 22:00:40 +0900

    Fix default argument to start freshly (#5)

--- bin/vusted.bat
+++ bin/vusted.bat
@@ -2,7 +2,7 @@
 setlocal
 
 if "%VUSTED_NVIM%"== "" set VUSTED_NVIM=nvim
-if "%VUSTED_ARGS%"== "" set VUSTED_ARGS=--headless -u NONE -i NONE -n
+if "%VUSTED_ARGS%"== "" set VUSTED_ARGS=--headless --clean
 
 %VUSTED_NVIM% %VUSTED_ARGS% ^
     --cmd "execute 'source ' .. fnamemodify(resolve('%~dp0'), ':h') .. '/vusted_entry.vim'" ^
